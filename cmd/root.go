6310a4ee098fc9d7de4f115838481275f5d69bf6
Florian Rand <florian.rand@gmail.com>
2022-04-06 16:37:04 +0200

    Remove Log.Panic
    If error then return. This fix two things, one, a duplicate unknown
    command (see this https://github.com/spf13/cobra/issues/1415), second,
    unnecesary exit(1) on command line

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,14 @@
 package cmd
 
 import (
-	"log"
-
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{Use: "cfm.sh"}
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		log.Panic(err)
+	err := rootCmd.Execute()
+	if err != nil {
+		return
 	}
 }
